a1b8420982f8066e6712981f4806c7d83026695f
PatrickL32 <Jedi.92.lewis@gmail.com>
2025-04-12 18:26:20 -0700

    Massive push with changes up to [date]

--- scripts/register.js
+++ scripts/register.js
@@ -5,18 +5,20 @@ let inputName=document.getElementById("txtName");
 let inputAge =document.getElementById("txtAge");
 let inputGender =document.getElementById("txtGender");
 let inputBreed =document.getElementById("txtBreed");
+let inputService=document.getElementById("txtservice");
 
 //object constructor
-function Pet( name,age,gender,Breed){
+function Pet( name,age,gender,breed, service){
     this.name=name;
     this.age=age;
     this.gender=gender;
-    this.Breed=Breed;//add breed property
+    this.Breed=breed;
+    this.service=service;
 }
 
 function register(){
     //create the newPet
-    let newPet=new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value);
+    let newPet=new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value);
     //push the newPet
     pets.push(newPet);
     //display the pets
@@ -24,19 +26,32 @@ function register(){
 }
 
 //display name function
-    function displayNames() {
-        document.getElementById("results").innerHTML = ""; // Clear previous results
-        for (let i = 0; i < pets.length; i++) {
-            document.getElementById("results").innerHTML += 
-                `<li>${pets[i].name} - ${pets[i].age} - ${pets[i].gender} - ${pets[i].Breed}</li>`;
-        }
-    }
+function displayNames() {
+    document.getElementById("results").innerHTML = ""; // Clear previous results
+    let result = "";
 
+    for (let i = 0; i < pets.length; i++) {
+        //document.getElementById("results").innerHTML += ""
+    
+        result+= `
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${pets[i].name}-<span class="text-secondary">${pets[i].service}</span></h5>
+                    <h6> ${pets[i].Breed}</h6>
+                    <p class="card-text" >${pets[i].gender},${pets[i].age}</p>
+                    <a href="#" class="btn btn-primary btn-sm">Delete</a>
+                </div>
+            </div>
+        `
+    }
+    document.getElementById("results").innerHTML=result;
+
+}
 function init(){
     // create pet objects
-    let pet1=new Pet("Fluffy",7,"Female","cat");
-    let pet2=new Pet("Mittens",4,"Female","cat");
-    let pet3= new Pet ("princess",6,"Female","dog");
+    let pet1=new Pet("Fluffy",7,"Female","Siberian","nailtrimming,brushing and bath");
+    let pet2=new Pet("Mittens",4,"Female","tuxedo","nailtrimming and deluxe brushing");
+    let pet3= new Pet ("princess",6,"Female","yorkie","grooming and teeth cleaning");
     //push the pets in the array
     pets.push(pet1,pet2,pet3);
     //display names
